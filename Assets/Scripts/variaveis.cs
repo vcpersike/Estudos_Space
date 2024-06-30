6e166165e8284bbb8669dd162fc5c249257a60ee
vcpersike <vcpersike@gmail.com>
2024-06-30 20:03:25 -0300

    TEste

---
+++ Assets/Scripts/variaveis.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class variaveis : MonoBehaviour
+{
+    int lifePlay = 10;
+    int atackEnemy = 2;
+
+    void Start()
+    {
+          
+    }
+
+    void Update()
+    {
+        
+    }
+}
