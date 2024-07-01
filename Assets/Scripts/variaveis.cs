530bd5af569e0677ce7b405dcc9f3a51cd498b2a
vcpersike <vcpersike@gmail.com>
2024-06-30 23:54:05 -0300

    Ajustes

--- Assets/Scripts/variaveis.cs
+++ Assets/Scripts/variaveis.cs
@@ -2,18 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class variaveis : MonoBehaviour
+public class SquareMovement : MonoBehaviour
 {
-    int lifePlay = 10;
-    int atackEnemy = 2;
-
-    void Start()
-    {
-          
-    }
+    public float moveSpeed = 5f; // Velocidade de movimento
+    public float rotationSpeed = 100f; // Velocidade de rotação
 
     void Update()
     {
-        
+        // Movimentação com teclas WASD
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveX, moveY);
+        transform.Translate(movement * moveSpeed * Time.deltaTime, Space.World);
+
+        // Rotação com setas
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(-Vector3.forward * rotationSpeed * Time.deltaTime);
+        }
     }
 }
